9c69042eac4f2c5d1c683f43250236f732097d7e
Martin Olague <molague23@hotmail.com>
2024-03-08 14:46:39 -0700

    Started the left Navbar

---
+++ app/properties/[id]/page.tsx
@@ -0,0 +1,9 @@
+//prettier-ignore
+'use client'
+import React from 'react';
+
+const PropertiesPage = () => {
+  return <div>PropertiesPage</div>;
+};
+
+export default PropertiesPage;
--- app/properties/page.tsx
+++ app/properties/page.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Link from 'next/link';
 
-const HomePage = () => {
+const PropertiesPage = () => {
+  console.log('hello');
   return (
     <div>
       <h1 className="text-3xl">Welcome</h1>
-      <Link href="/properties">Show Properties</Link>
+      <Link href="/">Go Home</Link>
     </div>
   );
 };
 
-export default HomePage;
+export default PropertiesPage;
