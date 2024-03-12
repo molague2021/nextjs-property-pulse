75d1fcc165c83474421af1b363adb778374ed426
Martin Olague <molague23@hotmail.com>
2024-03-11 22:39:46 -0600

    Added HomePage components Hero and Footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import '@/assets/styles/globals.css';
-import Navbar from '@/components/Navbar';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer/Footer';
 
 export const metadata = {
   title: 'PropertyPulse | Find The Perfect Rental',
@@ -14,6 +15,7 @@ const MainLayout = ({ children }) => {
       <body>
         <Navbar />
         <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
